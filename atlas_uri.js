7cea54c5aeec80afd27089664ec5017d0ea92eea
Innunekko <nitinc3114@gmail.com>
2024-11-28 15:35:15 +0530

    nodemod

---
+++ atlas_uri.js
@@ -0,0 +1,1 @@
+module.exports=uri= 'mongodb+srv://nitinc3114:Ncr3114@cluster0.nqipl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
--- server/index.js
+++ server/index.js
@@ -3,12 +3,21 @@ import mongoose from 'mongoose';
 
 const app = express();
 const PORT = 3000;
-
+const {MongoClient}= require("mongodb")
+const uri = require("./atlas_uri")
+console.log(uri)
+const client = new MongoClient(uri)
+const dbName = "fitfit";
+const connectToDatabase=async() =>{
+  try{
+    await client.connect();
+  }
+}
 app.use(express.json());
 
 // Connect to MongoDB
 mongoose.connect('mongodb://localhost:27017/fitness-tracker')
-  .then(() => console.log('Connected to MongoDB'))
+  .then(() => console.log('MongoDB'))
   .catch(err => console.error('MongoDB connection error:', err));
 
 // Basic route
